b869d600ce28e588a37c6017b427faeb2607cf5b
Supratik De <supratikde2@gmail.com>
2024-10-02 20:23:42 +0530

    update

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='w-full h-[100vh] flex flex-col justify-center gap-y-[20px]'>
+         <div className='bg-slate-800 text-red-400 border-blue-400 border-[12px] text-4xl font-bold flex justify-center'>page</div>
+         <div className='bg-slate-800 text-red-400 border-blue-400 border-[12px] text-4xl font-bold flex justify-center'>page</div>
+         <div className='bg-slate-800 text-red-400 border-blue-400 border-[12px] text-4xl font-bold flex justify-center'>page</div>
+    </div>
+   
+  )
+}
+
+export default page;
