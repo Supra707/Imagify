23ad63b2cda561f332237a7ed403f2bb46610ab3
Supratik De <supratikde2@gmail.com>
2024-10-02 22:16:56 +0530

    good

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,40 @@
-import React from 'react'
-
+"use client";
+import React, { useEffect, useState } from "react";
 const page = () => {
+  const [userinput, setuserinput] = useState("");
+  const submit_handler = (e) => {
+    e.preventDefault();
+    if (userinput !== "") {
+      console.log(userinput);
+    }
+    else {
+      alert('enter something you user!');
+    }
+    setuserinput("");
+  };
+  const handlechange = (e) => {
+    setuserinput(e.target.value);
+  };
+
   return (
-    <div className='w-full h-[100vh] flex flex-col justify-center gap-y-[20px]'>
-         <div className='bg-slate-800 text-red-400 border-blue-400 border-[12px] text-4xl font-bold flex justify-center'>page</div>
-         <div className='bg-slate-800 text-red-400 border-blue-400 border-[12px] text-4xl font-bold flex justify-center'>page</div>
-         <div className='bg-slate-800 text-red-400 border-blue-400 border-[12px] text-4xl font-bold flex justify-center'>page</div>
+    <div className="w-full h-[100vh] flex flex-col justify-start gap-y-[20px] m-5">
+      <div className="flex justify-center">
+        <form onSubmit={submit_handler}>
+          <div className="flex justify-center gap-x-4 items-center">
+            <input
+              className="border-black border-2 w-[392px] p-4 h-full rounded-md"
+              placeholder="Enter the prompt! Go Wild!"
+              value={userinput}
+              onChange={handlechange}
+            ></input>
+            <button className="p-4 w-fit h-full bg-yellow-400 font-bold transition-all duration-500 hover:bg-yellow-300  hover:font-bold hover:text-blue-950 hover:text-xl hover:rounded-md">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-   
-  )
-}
+  );
+};
 
 export default page;
